555423394d625c73a63e87ffeb83d2ad68d83c7e
Faten Eid <87611899+fateneid@users.noreply.github.com>
2024-11-07 00:50:12 +0200

    P32

--- P32_IsVowel.h
+++ P32_IsVowel.h
@@ -2,22 +2,50 @@
 
 #include <iostream>
 #include <string> 
+#include <cctype>
 using namespace std;
 
 namespace P32_IsVowel {
 
-	string ReadString(string Message) {
-		string MyString;
+	char ReadCharcter(string Message) {
+		char MyChar;
 		cout << Message;
-		getline(cin, MyString);
-		return MyString;
+		cin >> MyChar;
+		return MyChar;
 	}
 
+	bool IsVowel(char MyChar) {
+		char arrVowelChar[5] = { 'a','e','i','o','u' };
+		for (short i = 0; i < 5; i++) {
+			if (arrVowelChar[i] == tolower(MyChar)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool IsVowelAnotherSol(char MyChar) {
+		MyChar = tolower(MyChar);
+		return((MyChar == 'a' || MyChar == 'e' || MyChar == 'i' || MyChar == 'o' || MyChar == 'u'));
+	}
 
 	void Task() {
 
-	
+		char MyChar = ReadCharcter("Please Enter a Charcter?\n");
+
+		if (IsVowelAnotherSol(MyChar)) {
+			cout << "\nYES Letter '" << MyChar << "' is a vowel" << endl;
+		}
+		else {
+			cout << "\nNO Letter '" << MyChar << "' is NOT a vowel" << endl;
+		}
+
 
 	}
 
 }
+
+
+
+
+
