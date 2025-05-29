b89a49ead0c8b86ab929d829f4057421a8c545ba
Faten Eid <mulan.tech.914h@gmail.com>
2025-05-29 04:28:20 +0300

    remove unnecessary lines and comments

--- 07-algorithms-and-problem-solving-level-3.cpp
+++ 07-algorithms-and-problem-solving-level-3.cpp
@@ -1,54 +1,4 @@
 #include <iostream>
-#include "P01_3x3RandomMatrix.h"
-#include "P02_SumEachRowInMatrix.h"
-#include "P03_SumEachRowInMatrixInArray.h"
-#include "P04_SumEachColInMatrix.h"
-#include "P05_SumEachColInMatrixInAnotherArray.h"
-#include "P06_3x3OrderedMatrix.h"
-#include "P07_TransposeMatrix.h"
-#include "P08_MultiplyTwoMatrices.h"
-#include "P09_PrintMiddleRowAndColOfMatrix.h"
-#include "P10_SumOfMatrix.h"
-#include "P11_CheckMatricesEquality.h"
-#include "P12_CheckTypicalMatrices.h"
-#include "P13_CheckIdentityMatrix.h"
-#include "P14_CheckScalarMatrixh.h"
-#include "P15_CountNumberInMatrix.h"
-#include "P16_CheckSparseMatrix.h"
-#include "P17_NumberExistsInMatrix.h"
-#include "P18_IntersectedNumbersInMatrices.h"
-#include "P19_MinAndMaxNumberInMatrix.h"
-#include "P20_PalindromeMatrix.h"
-#include "P21_FibonacciSeries.h"
-#include "P22_FibonacciSeriesWithRecursion.h"
-#include "P23_PrintFirstLetterOfEachWord.h"
-#include "P24_UpperFirstLetterOfEachWord.h"
-#include "P25_LowerFirstLetterOfEachWord.h"
-#include "P26_UpperAndLowerAllLettersOfAString.h"
-#include "P27_InvertCharcterCase.h"
-#include "P28_InvertAllLettersCase.h"
-#include "P29_CountSmallAndCapitalLetters.h"
-#include "P30_CountLetters.h"
-#include "P31_CountLettersMatchCase.h"
-#include "P32_IsVowel.h"
-#include "P33_CountVowel.h"
-#include "P34_PrintAllVowelsInString.h"
-#include "P35_PrintEachWordInString.h"
-#include "P36_CountEachWordInString.h"
-#include "P37_SplitString.h"
-#include "P38_TrimLeftTrimRightTrim.h"
-#include "P39_JoinString.h"
-#include "P40_JoinStringOverloading.h"
-#include "P41_ReverseWords.h"
-#include "P42_ReplaceWords.h"
-#include "P43_ReplaceWordsCustom.h"
-#include "P44_RemovePunctuations.h"
-#include "P45_ConvertRecordToLine.h"
-#include "P46_ConvertLineDataToRecord.h"
-#include "P47_AddClientsToFile.h"
-#include "P48_ShowAllClients.h"
-#include "P49_FindClientByAccountNumber.h"
-#include "P50_DeleteClientByAccountNumber.h"
 #include "P51_UpdateClientByAccountNumber.h"
 
 int main()
@@ -58,17 +8,5 @@ int main()
     P51_UpdateClientByAccountNumber::Task();
 
 
-
 }
 
-/*
-
-library's namespace name = library name
-
-To run any problem:
-
-    - include its library
-    - use its namespace with any method
-    - run Task fun under its namespace
-
-*/
